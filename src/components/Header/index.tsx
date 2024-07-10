0f28f23761ef7d20e44f85abe1e3baed8ab8d529
diagonalbeam <beichuntanqiu@163.com>
2024-07-10 15:43:25 +0800

    refactor: update logo size in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ export function Header() {
       <header className="absolute flex justify-center top-0 w-screen z-50">
         <div className="flex justify-between w-[937px] h-[76px] items-center">
           <a href="/">
-            <img src={IMAGE_LOGO} className="h-5" alt="logo" />
+            <img src={IMAGE_LOGO} className="h-10" alt="logo" />
           </a>
           <nav className="flex items-center text-white text-16 font-semibold hover:opacity-80 gap-7">
             <a className={location.pathname === '/' ? activeClassName : ''} href="/">{t('home')}</a>
