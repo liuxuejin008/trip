f9c4d953e1a0023a33ae2bbc494276c74e57fc69
diagonalbeam <beichuntanqiu@163.com>
2024-07-16 14:51:20 +0800

    refactor: Update header component to include login or register link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -26,8 +26,8 @@ export function Header() {
           <nav className="flex items-center text-white text-16 font-semibold gap-7">
             <a className={cs('uppercase hover:opacity-80', location.pathname === '/' ? activeClassName : '')} href="/">{t('home')}</a>
             <a className="uppercase hover:opacity-80" href='/'>{t('contactUs')}</a>
-            {!isLogin && <a className="uppercase cursor-pointer hover:opacity-80" onClick={onLogin}>{t('loginOrRegister')}</a>}
             <Lang />
+            {!isLogin && <a className="uppercase cursor-pointer hover:opacity-80" onClick={onLogin}>{t('loginOrRegister')}</a>}
             <UserDropdown />
           </nav>
         </div>
